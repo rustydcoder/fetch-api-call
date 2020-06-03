cff96e6bf6137237df0193780a709f3a6a7499e7
Emmanuel George <rustydcoder@gmail.com>
2020-06-03 21:56:26 +0100

    use of es6 async/await to clean code

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 let foodArr = ["shrimps.jpg", "tacos.jpg", "burger.jpg"]
 
-console.log("about to fetch burger");
-for (const food of foodArr) {
-   fetch(food)
-      .then(res => res.blob())
-      .then(blob => {
-         const source = URL.createObjectURL(blob);
-         const img = new Image()
-         img.src = source
-         img.width = 200
-         document.body.append(img)
-         console.log(img)
-      })
-      .catch(err => {
-         console.log("error")
-         console.log(err)
-      })
+async function fetchImages(imgs) {
+   for (const img of imgs) {
+      const res = await fetch(img)
+      const blob = await res.blob()
+      const image = new Image()
+      image.src = URL.createObjectURL(blob)
+      image.width = 200
+      document.body.appendChild(image)
+   }
 }
-console.log("completed");
+
+fetchImages(foodArr)
