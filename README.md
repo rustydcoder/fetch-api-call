93144d47a088d074a4eff2295eba854f3a63ef1d
Emmanuel George <rustydcoder@gmail.com>
2020-06-04 11:38:15 +0100

    using chart to display .csv

---
+++ README.md
@@ -0,0 +1,19 @@
+# Working With APIs
+
+This is a project of working with different format of datas like:
+
+- text
+- blob
+- csv
+
+As you explore this repos, different commits for different data types
+
+#### RESOURCES
+
+[NASA](https://data.giss.nasa.gov/gistemp)
+
+[UNSPLASH](https://unsplash.com)
+
+[chartJs](https://chartjs.org)
+
+[Tutorial video](https://www.youtube.com/playlist?list=PLRqwX-V7Uu6YxDKpFzf_2D84p0cyk4T7X)
--- app.js
+++ app.js
@@ -1,14 +1,65 @@
-// DATA: https://data.giss.nasa.gov/gistemp/
-// CSV:  https://data.giss.nasa.gov/gistemp/tabledata_v4/ZonAnn.Ts+dSST.csv
+async function init() {
+   const xs = []
+   const ys = []
 
-getData()
-async function getData() {
    const response = await fetch('ZonAnn.Ts+dSST.csv')
    const data = await response.text()
    const table = data.split('\n').slice(1)
+
    table.forEach(row => {
       const cell = row.split(',')
-      const [year, temp, kuda, ...rest] = cell
-      console.log(year, temp, kuda)
+      const [year, temperature] = cell
+      xs.push(year)
+      ys.push(parseFloat(temperature) + 14)
+   })
+   drawChart(xs, ys)
+}
+
+
+function drawChart(xs, ys) {
+   const ctx = document.getElementById('myChart').getContext('2d')
+   let myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+         labels: [...xs],
+         datasets: [
+            {
+               label: 'Years',
+               fill: false,
+               backgroundColor: 'rgba(255, 99, 132, 0.2)',
+               borderColor: 'rgba(255, 99, 132, 1)',
+               borderWidth: 1,
+               data: [...ys]
+            }]
+      },
+      options: {
+         legend: {
+            labels: {
+               boxWidth: 0,
+            },
+            padding: {
+               left: 0,
+               right: 0,
+               top: 0,
+               bottom: 0
+            },
+            position: "bottom",
+         },
+         title: {
+            display: true,
+            text: 'Chart Of Zonal Annual Means In °C'
+         },
+         scales: {
+            yAxes: [{
+               ticks: {
+                  callback: function (value, index, values) {
+                     return value + '°';
+                  }
+               }
+            }]
+         }
+      }
    })
 }
+
+init()
