3a73756cbd2903b3b8eb123a2076d987798e8dff
Emmanuel George <rustydcoder@gmail.com>
2020-06-05 12:29:29 +0100

    updated and refactored to handle more data

--- app.js
+++ app.js
@@ -1,65 +1,123 @@
-async function init() {
-   const xs = []
-   const ys = []
+const cordinates = {
+   xs: [],
+   ys: {
+      mT: [],
+      nH: [],
+      sH: []
+   }
+}
 
-   const response = await fetch('ZonAnn.Ts+dSST.csv')
+async function init() {
+   const csv_url = 'ZonAnn.Ts+dSST.csv'
+   const response = await fetch(csv_url)
    const data = await response.text()
    const table = data.split('\n').slice(1)
 
    table.forEach(row => {
       const cell = row.split(',')
-      const [year, temperature] = cell
-      xs.push(year)
-      ys.push(parseFloat(temperature) + 14)
+      const [year, ...rest] = cell
+      cordinates.xs.push(year)
+      cordinates.ys.mT.push(parseTemp(rest[0]))
+      cordinates.ys.nH.push(parseTemp(rest[1]))
+      cordinates.ys.sH.push(parseTemp(rest[2]))
    })
-   drawChart(xs, ys)
 }
 
+async function drawChart({ xs, ys }) {
+   await init()
 
-function drawChart(xs, ys) {
+   let colors = ['rgb(255, 99, 132)', 'rgb(255, 159, 64)', 'rgb(255, 205, 86)']
+
    const ctx = document.getElementById('myChart').getContext('2d')
-   let myChart = new Chart(ctx, {
+   let config = {
       type: 'line',
       data: {
-         labels: [...xs],
+         labels: xs,
          datasets: [
             {
-               label: 'Years',
+               label: 'Means',
                fill: false,
-               backgroundColor: 'rgba(255, 99, 132, 0.2)',
-               borderColor: 'rgba(255, 99, 132, 1)',
-               borderWidth: 1,
-               data: [...ys]
-            }]
+               backgroundColor: colors[0],
+               borderColor: colors[0],
+               data: ys.mT,
+            }
+         ]
       },
       options: {
-         legend: {
-            labels: {
-               boxWidth: 0,
-            },
-            padding: {
-               left: 0,
-               right: 0,
-               top: 0,
-               bottom: 0
-            },
-            position: "bottom",
+         responsive: true,
+         tooltips: {
+            mode: 'index',
+            intersect: false,
+         },
+         hover: {
+            mode: 'nearest',
+            intersect: true
          },
          title: {
             display: true,
             text: 'Chart Of Zonal Annual Means In °C'
          },
          scales: {
+            xAxes: [{
+               display: true,
+               scaleLabel: {
+                  display: true,
+                  labelString: 'Years'
+               }
+            }],
             yAxes: [{
                ticks: {
                   callback: function (value, index, values) {
                      return value + '°';
-                  }
+                  },
+               },
+               display: true,
+               scaleLabel: {
+                  display: true,
+                  labelString: 'Temperature'
                }
             }]
+         },
+         elements: {
+            point: {
+               pointStyle: 'crossRot'
+            }
          }
+      }
+   }
+
+   const myChart = new Chart(ctx, config)
+
+   let i = 0;
+   document.getElementById('add').addEventListener('click', () => {
+      let addOns = [].concat([ys.nH], [ys.sH])
+      let labelling = ['Northern Hemisphere', 'Southern Hemisphere']
+
+      let newDataSet = {
+         label: labelling[i],
+         fill: false,
+         backgroundColor: colors[i + 1],
+         borderColor: colors[i + 1],
+         data: addOns[i],
+         borderDash: [5, 5]
+      }
+      if (config.data.datasets.length < Object.keys(ys).length) {
+         config.data.datasets.push(newDataSet)
+         i++
+      }
+      myChart.update()
+   })
+
+   document.getElementById('remove').addEventListener('click', () => {
+      if (i > 0) {
+         config.data.datasets.splice(-1, 1)
+         i--
+         myChart.update()
       }
    })
 }
 
-init()
+function parseTemp(n) {
+   return parseFloat(n) + 14
+}
+drawChart(cordinates)
